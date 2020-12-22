a964b87b60532b27d4c03be5e6f3375af177a5cf
iliaIvanov <ivanovilia96@mail.ru>
2020-12-22 16:20:33 +0300

    Revert "simple binary search"
    This reverts commit 45670b83

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 
-function sum(sumArr: number[] = [], num: number): boolean {
-    if (num === sumArr[0]) {
-        return true
-    }
-
-    if(sumArr.length===1){
-        return false
-    }
-
-    const firstPart = sumArr.slice(0, Math.floor(sumArr.length / 2))
-    const secondPart = sumArr.slice(Math.floor(sumArr.length / 2))
-
-    if (  num > firstPart[firstPart.length - 1]) {
-       return  sum(secondPart,num)
-    } else {
-      return   sum(firstPart,num)
-    }
-}
+import ToDoList from "./components/ToDoList";
 
 
-function App(this: void): JSX.Element {
+
+
+function App():JSX.Element {
     return (
-        <div>  {sum( [7,5,11,32].sort((a, b) => a - b), 32).toString()}</div>
+        <Router>
+            <Switch>
+                <Route path={'/'}>
+                    <ToDoList/>
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
