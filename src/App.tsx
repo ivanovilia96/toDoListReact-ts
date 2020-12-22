45670b83a3a07269694774fe82f2a219c2751eb1
iliaIvanov <ivanovilia96@mail.ru>
2020-12-22 16:09:57 +0300

    simple binary search

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 
-import ToDoList from "./components/ToDoList";
+function sum(sumArr: number[] = [], num: number): boolean {
+    if (num === sumArr[0]) {
+        return true
+    }
+
+    if(sumArr.length===1){
+        return false
+    }
+
+    const firstPart = sumArr.slice(0, Math.floor(sumArr.length / 2))
+    const secondPart = sumArr.slice(Math.floor(sumArr.length / 2))
+
+    if (  num > firstPart[firstPart.length - 1]) {
+       return  sum(secondPart,num)
+    } else {
+      return   sum(firstPart,num)
+    }
+}
 
 
-
-
-function App():JSX.Element {
+function App(this: void): JSX.Element {
     return (
-        <Router>
-            <Switch>
-                <Route path={'/'}>
-                    <ToDoList/>
-                </Route>
-            </Switch>
-        </Router>
+        <div>  {sum( [7,5,11,32].sort((a, b) => a - b), 32).toString()}</div>
     );
 }
 
